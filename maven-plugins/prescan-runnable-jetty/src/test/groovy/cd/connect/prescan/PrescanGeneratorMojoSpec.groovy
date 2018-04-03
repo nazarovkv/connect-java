17cf7dc4883bb4f43b11944bbb0a4eb006ff292a
Richard Vowles <richard@bluetrainsoftware.com>
2018-04-03 13:47:08 +1200

    - remove global classpath scanner and use fast-classpath-scanner which stores data in itself. this makes it reliable for multithreaded and cumulative use.

--- maven-plugins/prescan-runnable-jetty/src/test/groovy/cd/connect/prescan/PrescanGeneratorMojoSpec.groovy
+++ maven-plugins/prescan-runnable-jetty/src/test/groovy/cd/connect/prescan/PrescanGeneratorMojoSpec.groovy
@@ -1,44 +1,50 @@
 package cd.connect.prescan
 
-import com.bluetrainsoftware.classpathscanner.ClasspathScanner
 import spock.lang.Specification
 
 class PrescanGeneratorMojoSpec extends Specification{
 
 	void "We scan for our artifacts"(){
 		given: "we have a scanner and a classloader"
-			ClasspathScanner scanner = ClasspathScanner.getInstance()
 			ClassLoader loader = getClass().getClassLoader()
-			PrescanGeneratorMojo mojo = new PrescanGeneratorMojo()
+			PrescanGeneratorMojo mojo = new PrescanGeneratorMojo() {
+				@Override
+				protected File getOutputDirectoryFile() {
+					return new File( getClass().getResource('/' ).getFile() )
+				}
+			}
 			mojo.projectBuildDir = new File( getClass().getResource('/' ).getFile() )
 
 		when: "we perform a scan"
-			List<String> found = mojo.scan( scanner, loader )
+			Set<String> found = mojo.scan( loader )
 
 		then: "we expect to find stuff"
-			found.find { 'webxml=file:/WEB-INF/web.xml' == it }
-			found.find { 'resource=file:/META-INF/resources/' == it }
-			!found.find { 'resource=file:/META-INF/resources/empty.txt' == it }
-			found.find { 'fragment=file:/META-INF/web-fragment.xml' == it }
-			found.find { 'webxml=file:/META-INF/resources/WEB-INF/web.xml' == it }
+			found.find { 'webxml=jar:file:/WEB-INF/web.xml' == it }
+			found.find { 'resource=jar:file:/META-INF/resources/' == it }
+			!found.find { 'resource=jar:file:/META-INF/resources/empty.txt' == it }
+			found.find { 'fragment=jar:file:/META-INF/web-fragment.xml' == it }
+			found.find { 'webxml=jar:file:/META-INF/resources/WEB-INF/web.xml' == it }
 	}
 
 	void "We scan our jars for artifacts"(){
 		given: "we have a scanner and a classloader"
-			ClasspathScanner scanner = ClasspathScanner.getInstance()
-			ClassLoader loader = new URLClassLoader( asURLArray( [ '/jars/binks.jar', '/jars/fragment.jar' ] ) )
-			PrescanGeneratorMojo mojo = new PrescanGeneratorMojo()
-			mojo.projectBuildDir = new File( getClass().getResource('/' ).getFile() )
+			ClassLoader loader = new URLClassLoader( asURLArray( [ '/jars/binks.jar', '/jars/fragment.jar' ] ), (ClassLoader)null )
+			PrescanGeneratorMojo mojo = new PrescanGeneratorMojo()  {
+				@Override
+				protected File getOutputDirectoryFile() {
+					return new File( getClass().getResource('/' ).getFile() )
+				}
+			}
 			mojo.jarpath = "lib"
 
 		when: "we perform a scan"
-			List<String> found = mojo.scan( scanner, loader )
+			Set<String> found = mojo.scan(loader )
 
 		then: "we expect to find stuff"
-			found.size() == 3
+			found.size() == 8
 			found.find { 'webxml=jar:file:/lib/binks.jar!/WEB-INF/web.xml' == it }
 			found.find { 'fragment=jar:file:/lib/fragment.jar!/META-INF/web-fragment.xml' == it }
-			found.find { 'resource=jar:file:/lib/fragment.jar!/META-INF/resources/' == it }
+			!found.find { 'resource=jar:file:/lib/fragment.jar!/META-INF/resources/' == it } 		// fragment resources should not be here as there is nothing in it
 	}
 
 	private static URL[] asURLArray( List<String> strings ) {
