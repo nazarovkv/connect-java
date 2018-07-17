df17503021b91d4630f32c85c0287dc954e43b67
Richard Vowles <richard@bluetrainsoftware.com>
2018-07-17 13:29:12 +1200

    - deal with exceptions in permissions

--- maven-plugins/sanitize-rds-plugin/src/main/groovy/cd/connect/aws/rds/RdsClone.groovy
+++ maven-plugins/sanitize-rds-plugin/src/main/groovy/cd/connect/aws/rds/RdsClone.groovy
@@ -70,7 +70,7 @@ class RdsClone {
 
 		return snapshots.DBSnapshots
 			.findAll({ (ignoreAutomated && it.getSnapshotType() != 'automated' ) || !ignoreAutomated })
-		  .collect({ it.getDBInstanceIdentifier()})
+		  .collect({ it.getDBSnapshotIdentifier()})
 	}
 
 	void createDatabaseInstanceFromSnapshot(String database, String snapshot, String vpc,
@@ -195,6 +195,7 @@ class RdsClone {
 			println "instances = ${instances}"
 			return instances?.DBInstances?.first()
 		} catch (Exception e) {
+			e.printStackTrace()
 			return null
 		}
 	}
