e32956b147bbc96c990c0024d039e370b324ddbd
Richard Vowles <richard@bluetrainsoftware.com>
2019-05-18 17:44:53 +1200

    CORS filter not configurable enough, make it more configurable.

--- libraries/jersey-common/src/main/java/cd/connect/jersey/common/CorsFilter.java
+++ libraries/jersey-common/src/main/java/cd/connect/jersey/common/CorsFilter.java
@@ -1,5 +1,8 @@
 package cd.connect.jersey.common;
 
+import cd.connect.app.config.ConfigKey;
+import cd.connect.app.config.DeclaredConfigResolver;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
@@ -8,12 +11,28 @@ import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 // from https://stackoverflow.com/questions/28065963/how-to-handle-cors-using-jax-rs-with-jersey
 
 @Provider
 @PreMatching
 public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
+  @ConfigKey("jersey.cors.headers")
+  List<String> allowedHeaders = Arrays.asList("Access-Control-Allow-Headers",
+    // Whatever other non-standard/safe headers (see list above)
+    // you want the client to be able to send to the server,
+    // put it in this list. And remove the ones you don't want.
+    "X-Requested-With, Authorization, Content-type, " +
+      "Accept-Version, Content-MD5, CSRF-Token");
+  private String headers[];
+
+  public CorsFilter() {
+    DeclaredConfigResolver.resolve(this);
+
+    headers = allowedHeaders.toArray(new String[0]);
+  }
 
   /**
    * Method for ContainerRequestFilter.
@@ -57,12 +76,7 @@ public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilt
       response.getHeaders().add("Access-Control-Allow-Credentials", "true");
       response.getHeaders().add("Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-      response.getHeaders().add("Access-Control-Allow-Headers",
-        // Whatever other non-standard/safe headers (see list above)
-        // you want the client to be able to send to the server,
-        // put it in this list. And remove the ones you don't want.
-        "X-Requested-With, Authorization, " +
-          "Accept-Version, Content-MD5, CSRF-Token");
+      response.getHeaders().addAll("Access-Control-Allow-Headers", headers);
     }
 
     // Cross origin requests can be either simple requests
